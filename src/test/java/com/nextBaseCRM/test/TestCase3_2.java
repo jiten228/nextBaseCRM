7d4cc36076cf7cc0aad4d6d4fb44832e1db8a26f
jiten228 <jiten228@yahoo.com>
2021-01-08 13:31:05 -0600

    testng Dependency added

--- src/test/java/com/nextBaseCRM/test/TestCase3_2.java
+++ src/test/java/com/nextBaseCRM/test/TestCase3_2.java
@@ -8,14 +8,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.security.Key;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Locale;
-import java.util.NoSuchElementException;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
-public class TestCase3_1 {
-    public static void main(String[] args) throws InterruptedException{
+public class TestCase3_2 {
+    public static void main(String[] args) throws InterruptedException {
 
         // Setup webdriver
         WebDriverManager.chromedriver().setup();
@@ -62,55 +59,36 @@ public class TestCase3_1 {
         }
 
         //*******************************************************************************
-        // Search (find people, documents) box & assign value "employee"+Enter Key
+        // Search (find people, documents) box & type any single alphabet either a-z / A-Z
         //*******************************************************************************
 /*
         WebElement searchBox = driver.findElement(By.xpath("//input[@id = 'search-textbox-input']"));
         searchBox.sendKeys("Time and Reports");
         Thread.sleep(2000);
         searchBox.sendKeys(Keys.ENTER);
-*/
+
         ArrayList<String> positiveData = new ArrayList<String>(Arrays.asList("Employees", "Tasks",
-            "Documents",
-                //"Time and Reports",
-                "Workgroups", "Mail", "Drive"));
+                "Documents", "Workgroups", "Mail", "Drive"));
 
         for (String each : positiveData) {
 
-            WebElement searchBox = driver.findElement(By.xpath("//input[@id = 'search-textbox-input']"));
-            searchBox.sendKeys(each);
-            Thread.sleep(3000);
-            searchBox.sendKeys(Keys.ENTER);
-            System.out.println("===========================================================");
-            boolean isLocate = driver.findElement(By.xpath("//input[@id = 'search-textbox-input']")).isDisplayed();
-            if (isLocate) {
+*/
+        WebElement searchBox = driver.findElement(By.xpath("//input[@id = 'search-textbox-input']"));
+        searchBox.sendKeys("z");
+        Thread.sleep(2000);
+        //searchBox.sendKeys(Keys.ENTER);
+        System.out.println("===========================================================");
+        boolean isLocate = driver.findElement(By.className("search-title-top-subtitle-text")).isDisplayed();
+        System.out.println("isLocate = " + isLocate);
+        WebElement getElement = driver.findElement(By.className("search-title-top-subtitle-text"));
+        String getValue = getElement.getText();
+        if ((getValue.equals("Employees")) || (getValue.equals("Groups")) || (getValue.equals("Search")) || getValue.equals("Menu items")) {
+            System.out.println("Key Alphabet PASSED! : ");
+        } else {
+            System.out.println("Key Word FAILED! For: ");
+        }
 
-                actualTitle = driver.getTitle();
-                if (actualTitle.toLowerCase().contains(each.toLowerCase())) {
-                    System.out.println("Key Word PASSED! : " + each);
-                } else {
-                    try {
-                        WebElement getElement = driver.findElement(By.className("structure-more-employee"));
-                        String getValue = getElement.getText();
-                        if (getValue.toLowerCase().contains(each.toLowerCase())) {
-                            System.out.println("Key Word PASSED! : " + each);
-                            //continue;
-                        }
-                    } catch (NoSuchElementException e) {
-                        e.getMessage();
-                    }
 
-                /*
-                try {
-                    WebElement getElement = driver.findElement(By.className("feed-wrap-empty"));
-                    if (getElement.getText().equals("Nothing found")) {
-                        System.out.println("Key Word FAILED! For: " + each);
-                    }
-                }catch (NoSuchElementException e){
-                    e.getMessage();
-                }
-                */
-                }
 
 /*
             Thread.sleep(2000);
@@ -120,8 +98,7 @@ public class TestCase3_1 {
 
             driver.findElement(By.name("USER_LOGIN")).clear(); //username
 */
-            }
-        }
+        //}
         driver.close();
     }
 }
